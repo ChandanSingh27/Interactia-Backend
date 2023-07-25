026f2990996ab7115742313f069d87098193956b
ChandanKumar Singh <cks917356@gmail.com>
2023-07-25 15:18:31 +0530

    cors set successfully

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const bodyparser = require('body-parser')
 const morgan = require('morgan')
 const { errorMiddleware } = require('./middlewares/error.js')
 const userRouter = require('./routes/usersRoutes.js')
+const cors = require('cors')
 const app = express()
 require('dotenv/config')
 
@@ -11,6 +12,11 @@ const api_version = process.env.API_VERSION
 //all middleware of the app here...
 app.use(bodyparser.json())
 app.use(morgan('dev'))
+app.use(cors({
+        credentials: true,
+        methods: ['GET,POST,PUT,DELETE'],
+        origin: ['*']
+}))
 
 //all routes of app...
 app.use(`${api_version}/users`,userRouter)
